a3b49583f7fea87ad501b7a258bb334f124a44ef
Tsukina-7mochi <tsukina7mochi@gmail.com>
2023-11-24 13:34:09 +0900

    build: fix bundle script

--- bundleSelf.lua
+++ bundleSelf.lua
@@ -4,10 +4,14 @@ bundle {
     entry = "src.neblua",
     output = "./dist/neblua.lua",
     files = {
+        "./src/getFileContent.lua",
+        "./src/moduleLoader.lua",
         "./src/neblua.lua",
         "./src/path.lua",
+        "./src/requireModule.lua",
         "./src/string.lua",
-        { path = "./src/templates/template.lua", type = "text" }
+        { path = "./version.txt",                type = "text" },
+        { path = "./src/templates/template.lua", type = "text" },
     },
     verbose = true,
 }
@@ -17,9 +21,13 @@ bundle {
     output = "./dist/neblua-cli.lua",
     files = {
         "./src/cli.lua",
+        "./src/getFileContent.lua",
+        "./src/moduleLoader.lua",
         "./src/neblua.lua",
         "./src/path.lua",
+        "./src/requireModule.lua",
         "./src/string.lua",
+        { path = "./version.txt",                type = "text" },
         { path = "./src/templates/template.lua", type = "text" }
     },
     verbose = true,
