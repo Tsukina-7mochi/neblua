9c7a2ac0056c2e1bfaeac56c50fc08f0729a576a
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-05-24 20:12:30 +0900

    refactor: implement option normalizer

--- src/neblua.lua
+++ src/neblua.lua
@@ -22,6 +22,87 @@ local substitutionPoints = {
 ---@field output string
 ---@field verbose? boolean
 
+---@class NormalizedBundleOptions
+---@field rootDir string
+---@field entry string
+---@field files { path: string, type: "lua" | "text" }[]
+---@field output string
+---@field verbose boolean
+
+---@param options BundleOptions
+---@return NormalizedBundleOptions
+local function normalizeBundleOptions(options)
+    if type(options) ~= "table" then
+        error("[neblua] Expected options to be a table")
+    end
+
+    local rootDir = options.rootDir
+    local entry = options.entry
+    local files = options.files
+    local output = options.output
+    local verbose = options.verbose
+
+    if rootDir == nil then
+        rootDir = "./"
+    else
+        if type(rootDir) ~= "string" then
+            error("[neblua] Expected options.rootDir to be a string or nil")
+        end
+
+        -- make rootDir to start with './' and end with '/'
+        if rootDir:sub(-1) ~= path.separator then
+            rootDir = rootDir .. path.separator
+        end
+        rootDir = path.relative(rootDir, ".")
+    end
+
+    if type(entry) ~= "string" then
+        error("[neblua] Expected options.entry to be a string")
+    end
+
+    if type(files) ~= "table" then
+        error("[neblua] Expected options.files to be a table")
+    end
+    for i, file in ipairs(files) do
+        if type(file) == "string" then
+            files[i] = { path = file, type = "lua" }
+        elseif type(file) == "table" then
+            if type(file.path) ~= "string" then
+                error("[neblua] Expected options.files[" .. i .. "].path to be a string")
+            elseif type(file.type) ~= "string" then
+                error("[neblua] Expected options.files[" .. i .. "].type to be one of {\"lua\", \"text\"}")
+            end
+        else
+            error("[neblua] Expected options.files[" .. i .. "] to be a string or a table")
+        end
+
+        files[i].path = path.relative(files[i].path, ".")
+        if files[i].type == "lua" then
+        elseif files[i].type == "text" then
+            --do nothing
+        else
+            error("[neblua] Expected options.files[" .. i .. "].type to be one of {\"lua\", \"text\"}")
+        end
+    end
+
+    if type(output) ~= "string" then
+        error("[neblua] Expected options.output to be a string")
+    end
+    options.output = path.relative(options.output, ".")
+
+    if verbose ~= nil and type(verbose) ~= "boolean" then
+        error("[neblua] Expected options.verbose to be a string or nil")
+    end
+
+    return {
+        rootDir = rootDir,
+        entry = entry,
+        files = files,
+        output = output,
+        verbose = verbose,
+    }
+end
+
 ---@param options BundleOptions
 local function bundle(options)
     local verbosePrint = function(...)
@@ -30,71 +111,11 @@ local function bundle(options)
         end
     end
 
-    if type(options) ~= "table" then
-        error("[neblua] Expected options to be a table")
-    end
-    if type(options.entry) ~= "string" then
-        error("[neblua] Expected options.entry to be a string")
-    end
-    if type(options.files) ~= "table" then
-        error("[neblua] Expected options.files to be a table")
-    end
-    if type(options.output) ~= "string" then
-        error("[neblua] Expected options.output to be a string")
-    end
+    local options = normalizeBundleOptions(options)
 
-    options.output = path.relative(options.output, ".")
-    if options.rootDir == nil then
-        options.rootDir = "./"
-    else
-        if type(options.rootDir) ~= "string" then
-            error("[neblua] Expected options.rootDir to be a string")
-        end
-
-        if options.rootDir:sub(-1) ~= path.separator then
-            options.rootDir = options.rootDir .. path.separator
-        end
-        options.rootDir = path.relative(options.rootDir, ".")
-    end
-
-    for i, file in ipairs(options.files) do
-        if type(file) == "string" then
-            options.files[i] = {
-                path = file,
-                type = "lua",
-            }
-        elseif type(file) == "table" then
-            if type(file.path) ~= "string" then
-                error("[neblua] Expected options.files[" .. i .. "].path to be a string")
-            elseif type(file.type) ~= "string" then
-                error("[neblua] Expected options.files[" .. i .. "].type to be a string")
-            end
-        else
-            error("[neblua] Expected options.files[" .. i .. "] to be a string or a table")
-        end
-
-        options.files[i].path = path.relative(options.files[i].path, ".")
-        local extName = path.extName(options.files[i].path)
-        if options.files[i].type == "lua" then
-            if extName ~= ".lua" then
-                error("[neblua] File " .. options.files[i] .. " is not a Lua file")
-            end
-        elseif options.files[i].type == "text" then
-            --do nothing
-        else
-            error("[neblua] Unknown file type: " .. options.files[i].type)
-        end
-    end
-
+    verbosePrint("Root  : " .. options.rootDir)
     verbosePrint("Entry : " .. options.entry)
     verbosePrint("Output: " .. options.output)
-    if options.verbose == true then
-        local files = {}
-        for _, file in ipairs(options.files) do
-            table.insert(files, string.format("[%s]%s", file.type, file.path))
-        end
-        verbosePrint("Files : " .. table.concat(files, ", "))
-    end
 
     ---@type table<string, { type: string, content: string }>
     local files = {}
